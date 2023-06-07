f9d1b29d907b8968bb57daba84bcc00b055508a4
Angelo D <43831545+AngeloD2022@users.noreply.github.com>
2023-06-07 00:07:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FocusPrivacy
+
+FocusPrivacy is a browser extension that prevents websites from knowing if the window loses focus.
---
+++ extension/js/content_script.js
@@ -0,0 +1,54 @@
+(function () {
+    console.log("FocusPrivacy: payload injected.");
+
+    let removedCallbacks = {
+        "blur": [],
+        "focus": []
+    }
+
+    function removeWindowListeners() {
+        // Remove all the blacklisted event listeners from the window.
+        for (let key in removedCallbacks) {
+            if (removedCallbacks[key].length === 0)
+                continue;
+
+            for (let callback of removedCallbacks[key]) {
+                window.removeEventListener(key, callback);
+            }
+        }
+    }
+
+    function monkeypatchWindow() {
+        // if the site uses window.onfocus or window.onblur, we need to overwrite the callbacks.
+        window.onfocus = function () {};
+        window.onblur = function () {};
+    }
+
+    function monkeypatchDocument(focused) {
+        document.isFocused = function () {
+            return focused;
+        };
+    }
+
+    // We need to first monkeypatch addEventListener to record any pertinent callback functions.
+    // This is necessary because getEventListeners() is only exposed to the console.
+    Window.prototype._addEventListener = Window.prototype.addEventListener;
+    Window.prototype.addEventListener = function (type, listener, options) {
+        if (type in removedCallbacks) {
+            // the reason we're removing them later as opposed to simply not adding them is because we want to
+            // give the option to reapply them/disable protection later in the UI.
+            removedCallbacks[type].push(listener);
+        }
+
+        this._addEventListener(type, listener, options);
+    }
+
+    monkeypatchDocument(true);
+
+    document.addEventListener("DOMContentLoaded",function (e) {
+        monkeypatchWindow();
+        removeWindowListeners();
+        console.log("FocusPrivacy: runtime patch complete.");
+    });
+
+})();
